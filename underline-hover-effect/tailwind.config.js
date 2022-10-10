88cd6eb2b7cdc4be8049452e36091308702a5a34
Adem Erbas <adem5434e@gmail.com>
2022-10-10 11:19:18 +0200

    underline-hover-effect

---
+++ underline-hover-effect/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.html"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'dancing': ['Dancing Script', 'cursive'],
+      }
+    },
+  },
+  plugins: [],
+}
